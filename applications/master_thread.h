44d230f49589ccf62072df3539a6ff84109bcf8d
balanceTWK <linuxtwk@outlook.com>
2018-05-19 16:57:06 +0800

    avoidObstacles5-19

--- applications/master_thread.h
+++ applications/master_thread.h
@@ -5,15 +5,22 @@
 #include <rthw.h>
 
 #define controlEventEaix4 (1 << 0)
+#define control1 (1 << 1)
+#define control2 (1 << 2)
+#define control3 (1 << 3)
 
 extern struct rt_event ControlEvent;
 extern struct rt_mutex SpeedMutex;
 extern struct rt_mutex Mpu9250Mutex;
+extern struct rt_mutex StatusMutex;
+extern struct rt_mutex DistanceMutex;
 
 extern volatile float mypitch, myroll, myyaw;
 
-extern rt_int16_t wantspeed1;
-extern rt_int16_t wantspeed2;
+extern volatile rt_int16_t wantspeed1;
+extern volatile rt_int16_t wantspeed2;
+extern volatile rt_uint32_t distance;
+extern volatile rt_uint32_t CarStatus;
 
 typedef struct
 {
